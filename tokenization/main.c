e9b412d7127b38a2ba972fc95c768dc3e33f9ac6
Rashed Mahdi Bukhary <rashedmahdi55@gmail.com>
2025-07-07 19:19:40 +0600

    main function implementation

---
+++ tokenization/main.c
@@ -0,0 +1,62 @@
+#include "defs.h"
+
+#define extern_             // defining extern_ makes sure, the global variable belongs to main only
+#include "data.h"           // other files will inherit them as extern
+#undef extern_
+
+#include "defs.h"
+
+// setting up initial condition
+static void init(void)
+{
+    line = 1;
+    putback = '\n'; 
+}
+
+// usage message if incorrect start
+static void usage(char* prog)
+{
+    fprintf(stderr, "Usage: %s [inFile]\n", prog);
+}
+
+// printable tokens
+char * tokenStr[] = {"+", "-", "*", "/", "INTLIT"};
+
+// scans the file and prints each found token
+static void scanFile(void)
+{
+    struct token t;
+
+    while(scan(&t))
+    {
+        printf("Token: %s", tokenStr[t.token]);     // prints each token found
+        if (t.token == T_INTLIT)
+        {
+            printf(", value: %d", t.intValue);
+        }
+
+        printf("\n");
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        usage(argv[0]);
+        exit(1);
+    }
+
+    
+    init();                         // setting up initial condition
+
+    if ((inFile = fopen(argv[1], "r")) == NULL)     // checks if the file is opened correctly
+    {
+        fprintf(stderr, "Unable to open: %s\n", argv[1]);
+        exit(1);
+    }
+
+    scanFile();                     // scans the 
+
+    exit(0);
+}
